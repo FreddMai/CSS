2cc74caeb4cee415bc428b1d1343d826bf96b3ce
Freddy M <fredtein@gmail.com>
2020-09-23 11:39:56 +0300

    Adding transitions and animations

--- shared.js
+++ shared.js
@@ -4,11 +4,15 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
     modal.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function () {
+      backdrop.classList.add("open");
+    }, 10);
   });
 }
 
@@ -26,9 +30,21 @@ function closeModal() {
     modal.classList.remove("open");
   }
   backdrop.classList.remove("open");
+  setTimeout(function () {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
 toggleButton.addEventListener("click", function () {
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function () {
+    backdrop.classList.add("open");
+  }, 10);
 });
+
+ctaButton.addEventListener("animationstart", function (event) {});
+
+ctaButton.addEventListener("animationend", function (event) {});
+
+ctaButton.addEventListener("animationiteration", function (event) {});
